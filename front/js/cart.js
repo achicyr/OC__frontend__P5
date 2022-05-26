462225e01e9f4c40b5af870a3a51333e50c3fea9
achicyr <hi.cyril@gmail.com>
2022-05-26 23:51:33 +0900

    WIP ÉTAPE 9 FAITE Il est maintenant possible de modifier la quantité d'un article à partir de la page panier Mais aussi de supprimer un article du panier
    - IL FAUT FAIRE DES COOOOMMMMMMEEEEENNTTTTTTAAAAAIRRRREEEEEEEE.....

--- front/js/cart.js
+++ front/js/cart.js
@@ -14,18 +14,40 @@ fetch("http://localhost:3000/api/products")
 })
 .then(function (products) {
 
-    console.log(products);
+    function getTotalAmount(){
+        if(panier.length != 0){
+            totalQuantity.innerHTML = panier
+                                        .map(x=>parseInt(x.quantity))
+                                        .reduce((x,y)=>x+y)
+            totalPrice.innerHTML = panier
+                                    .map(x=>x.price*x.quantity)
+                                    .reduce((x,y)=>x+y)
+        }else {
+            cart__items.innerHTML = "Votre panier est vide, veuillez ajouter des article avant de pouvoir passer commande."
+            totalQuantity.innerHTML = 0
+            totalPrice.innerHTML = 0
+        }
+    }
     
-    for(id in storage)
-        for(indexProduct in products)
-            if(id == products[indexProduct]._id){
-                for(color in storage[id]){
-                    products[indexProduct].color = color
-                    products[indexProduct].quantity = storage[id][color]
-                    panier.push({...products[indexProduct]})
+    function getPanier(){
+        panier = []
+        for(id in storage)
+            for(indexProduct in products)
+                if(id == products[indexProduct]._id){
+                    for(color in storage[id]){
+                        products[indexProduct].color = color
+                        products[indexProduct].quantity = storage[id][color]
+                        panier.push({...products[indexProduct]})
+                    }
                 }
-            }
-                
+    }      
+
+    
+    console.log(products);
+
+
+    getPanier()
+
     panier.map(item=>{
         cart__items.innerHTML += `
             <article class="cart__item" data-id="${item._id}" data-color="${item.color}">
@@ -52,12 +74,44 @@ fetch("http://localhost:3000/api/products")
         `
     })
 
-    totalQuantity.innerHTML = panier
-                                .map(x=>parseInt(x.quantity))
-                                .reduce((x,y)=>x+y)
-    totalPrice.innerHTML = panier
-                            .map(x=>x.price*x.quantity)
-                            .reduce((x,y)=>x+y)
+
+    getTotalAmount()
+    
+
+    let killBTN = document.querySelectorAll('.deleteItem')
+    Object.keys(killBTN).map(index=>killBTN[index].addEventListener("click",function(e){
+        let article = this.closest('article'),
+        id = article.dataset.id,
+        color = article.dataset.color,
+        item = JSON.parse(localStorage.getItem(id)),
+        doDelete = confirm(`Vous êtes sur le point de supprimer un produit(${article.getElementsByTagName('h2')[0].textContent}(${article.getElementsByClassName('itemQuantity')[0].value})) de votre panier, êtes-vous sûr ?`)
+        alert(id+" xxx "+color)
+        if(doDelete){
+            // console.log(item)
+            delete item[color]
+            // console.log(item)
+            localStorage.setItem(id, JSON.stringify(item))
+
+            document.location.reload()
+        }
+    }))
+
+    let quantityINPUT = document.querySelectorAll('.itemQuantity')
+    Object.keys(quantityINPUT).map(index=>quantityINPUT[index].addEventListener("blur",function(e){
+        let article = this.closest('article'),
+        id = article.dataset.id,
+        color = article.dataset.color,
+        item = JSON.parse(localStorage.getItem(id)),
+        doChange = item[color] != article.getElementsByClassName('itemQuantity')[0].value
+            ? confirm(`Voulez-vous vraiment changer la quantité de (${item[color]}) à (${article.getElementsByClassName('itemQuantity')[0].value}) pour le produit suivant ? (${article.getElementsByTagName('h2')[0].textContent}) de votre panier, êtes-vous sûr ?`)
+            : false
+        alert(id+" xxx "+color)
+        if(doChange){
+            item[color] = article.getElementsByClassName('itemQuantity')[0].value
+            localStorage.setItem(id, JSON.stringify(item))
+            document.location.reload()
+        }
+    }))
 })
 
 // let template = `
